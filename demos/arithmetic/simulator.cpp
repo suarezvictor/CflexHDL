8c3ca550dd38398378b89495c75a66c689cd14ed
Victor Suarez Rovere <suarezvictor@gmail.com>
2023-03-10 02:28:05 -0300

    tested arithmetic module with clang and with no coroutines

--- demos/arithmetic/simulator.cpp
+++ demos/arithmetic/simulator.cpp
@@ -7,18 +7,23 @@
 
 #include "cflexhdl.h"
 
+#if defined(CFLEX_NO_COROUTINES) && defined(CFLEX_VERILATOR)
+#error cosimulation needs coroutines, CFLEX_NO_COROUTINES should NOT be defined
+#endif
+
 //#define _DEBUG
 
 #ifdef _DEBUG
 #define ITERATIONS 30
 #define DISPLAY printf
 #else
-#ifndef CFLEX_VERILATOR
 #define ITERATIONS 100*1000*1000
-#else
-#define ITERATIONS 1*1000*1000
-#endif
 #define DISPLAY(...)
+#endif
+
+static long clk_count = 0;
+#ifdef CFLEX_NO_COROUTINES
+inline void wait_clk() {++clk_count;}
 #endif
 
 #include "arithmetic.cc" //algorithm implementation
@@ -28,21 +33,23 @@
 VM_arithmetic *top = new VM_arithmetic;
 #endif
 
-#ifdef CFLEX_NO_COROUTINES
-#error this requires coroutines, CFLEX_NO_COROUTINES should NOT be defined
-#endif
-
 int main()
 {
 
   clock_t start_time = clock();
-  long clk_count = 0;
+  uint16_t sum = 0;
   uint16_t a = 0, b = 0, result = 0xFFFF, v_result = 0xFFFF;
 
   for(int i = 0; i < ITERATIONS; ++i)
   {
+#ifdef CFLEX_SIMULATION
+#ifdef CFLEX_NO_COROUTINES
+    prod16x16_16(a, b, result);
+#else
     MODULE_TYPE m = prod16x16_16(a, b, result);
-    
+#endif
+#endif
+
 #ifdef CFLEX_VERILATOR  
     top->in_run = 0;
     top->clock = 0; top->eval();
@@ -61,14 +68,22 @@ int main()
       ++clk_count;
     }
 #endif
+
+#ifdef CFLEX_SIMULATION
+#ifndef CFLEX_NO_COROUTINES
     while(m.clock()) //execute all steps
     {
 #ifndef CFLEX_VERILATOR
       ++clk_count;
 #endif
     }
+#endif
+#else
+    result = v_result;
+#endif
 
     DISPLAY("A=0x%04X, B=0x%04X, C_RESULT=0x%04X, V_RESULT=0x%04X\n", a, b, result, v_result);
+    sum += result;
 
 #ifdef CFLEX_VERILATOR  
     if(result != v_result)
@@ -87,7 +102,7 @@ int main()
   delete top;
 #endif
 
-  printf("SIMULATION RESULTS: clock frequency %0.f MHz\n", clk_count*1e-6*CLOCKS_PER_SEC/dt);
+  printf("SIMULATION RESULTS: sum %hu, clock frequency %0.f MHz\n", sum, clk_count*1e-6*CLOCKS_PER_SEC/dt);
   return 0;
 }
 
