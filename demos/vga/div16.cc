6b94000fbff4bf006d7620983839bb2264da0a79
Victor Suarez Rovere <suarezvictor@gmail.com>
2023-03-10 13:39:33 -0300

    improve newton division algorithm

--- demos/vga/div16.cc
+++ demos/vga/div16.cc
@@ -1,15 +1,15 @@
 #include "cflexhdl.h"
 
-//#define CFLEX_USE_DIV_NEWTON
+#define CFLEX_USE_DIV_NEWTON //newton algorithm is faster when compiled
 
 #ifdef CFLEX_USE_DIV_NEWTON
 
-void fixed16_newton_initial_estimate(uint16 x, uint16& ret)
+uint16 fixed16_newton_initial_estimate(uint16 x)
 {
-  ret = x - (x>>4); //substracts 1/16 (so the factor is 15/16)
+  return x - (x>>4); //substracts 1/16 (so the factor is 15/16)
 }
 
-void fixed16_div_newton(uint16 D, uint16 X, uint16& ret)
+uint16 fixed16_div_newton(uint16 D, uint16 X)
 {
   uint16 a;
   int16 b;
@@ -17,41 +17,49 @@ void fixed16_div_newton(uint16 D, uint16 X, uint16& ret)
   a = (D*X) >> 16;
   b = (a^32767)+1;//0x8000u - a;
   c = X*b >> (16-1);
-  ret = X + c;
+  return X + c;
 }
 
-void fixed16_approx_reciprocal_bounded_half(uint16 D, uint16& ret) //0x8000=0.5 to 0xFFFF=~1.0, returns value/2
+uint16 fixed16_approx_reciprocal_bounded_half(uint16 D) //0x8000=0.5 to 0xFFFF=~1.0, returns value/2
 {
   uint16 X0;
   uint16 X1;
   uint16 X2;
-  uint16 X = D^0x7FFF; //approximate negative and offset, good enough for an initial guess
-  fixed16_newton_initial_estimate(X, X0);
-  fixed16_div_newton(D, X0, X1);
-  fixed16_div_newton(D, X1, X2);
-  ret = X2;
+  uint16 X3;
+  uint16 X = D^/*0x7FFF*/32767; //approximate negative and offset, good enough for an initial guess
+  X0 = fixed16_newton_initial_estimate(X);
+  X1 = fixed16_div_newton(D, X0);
+  X2 = fixed16_div_newton(D, X1);
+  return X2;
 }
 
-void fixed16_div_aprox_32(uint16 N, uint16 D, uint32& ret)
+uint32 fixed16_div_aprox_32(uint16 N, const uint16& D0)
 {
-  uint8 justify = 0;
-  uint32 R;
-  if((D & 0xFF00) == 0) { justify = justify | 8; D = D << 8; }
-  if((D & 0xF000) == 0) { justify = justify | 4; D = D << 4; }
-  if((D & 0xC000) == 0) { justify = justify | 2; D = D << 2; }
-  if((D & 0x8000) == 0) { justify = justify | 1; D = D << 1; }
-  R = N*fixed16_approx_reciprocal_bounded_half(D);
-  if((justify & 8) == 0) R = R >> 8;
-  if((justify & 4) == 0) R = R >> 4;
-  if((justify & 2) == 0) R = R >> 2;
-  if((justify & 1) == 0) R = R >> 1;
-  ret = R;
+  uint32 R0, R1, R2, R3, R4;
+  uint32 D1, D2, D3, D4;
+  uint1 C0, C1, C2, C3;
+  C0 = (D0 & (255<<8)) == 0;
+  D1 = C0 ? D0 << 8 : D0;
+  C1 = (D1 & (15<<12)) == 0;
+  D2 = C1 ? D1 << 4: D1;
+  C2 = (D2 & (3<<14)) == 0;
+  D3 = C2 ? D2 << 2: D2;
+  C3 = (D3 & (1<<15)) == 0;
+  D4 = C3 ? D3 << 1: D3; 
+  uint16 f;
+  f = fixed16_approx_reciprocal_bounded_half(D4);
+  R0 = N*f;
+  R1 = C0 ? R0 : R0 >> 8;
+  R2 = C1 ? R1 : R1 >> 4;
+  R3 = C2 ? R2 : R2 >> 2;
+  R4 = C3 ? R3 : R3 >> 1;
+  return R4;
 }
 
-MODULE div16(const uint16& num, const uint16& den, uint16& ret) //FIXME: correct parser/generator
+MODULE _div16(const uint16& num, const uint16& den, uint16& ret) //FIXME: correct parser/generator
 {
-  uint16 r;
-  fixed16_div_aprox_32(num, den, r);
+  uint32 r;
+  r = fixed16_div_aprox_32(num, den);
   ret = r >> 16;
 }
 
